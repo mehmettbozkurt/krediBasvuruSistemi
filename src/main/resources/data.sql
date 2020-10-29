2f5b658c2ef9b00a61dba8ab6b45438f3703ac68
Mehmet Bozkurt <mehmett.bozkurt1@gmail.com>
2020-10-30 00:10:23 +0300

    db update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,12 @@
 INSERT INTO user (name, identity, phone, in_come, credit_score, credit_amount, credit_status)
-VALUES  ('',20924544887,'',0,400,0,0);
+VALUES  ('',20924544885,'',0,400,0,0);
+INSERT INTO user (name, identity, phone, in_come, credit_score, credit_amount, credit_status)
+VALUES  ('',20924544886,'',0,450,0,0);
+INSERT INTO user (name, identity, phone, in_come, credit_score, credit_amount, credit_status)
+VALUES  ('',20924544887,'',0,550,0,0);
 
 INSERT INTO user (name, identity, phone, in_come, credit_score, credit_amount, credit_status)
-VALUES  ('',20924544888,'',0,550,0,0);
+VALUES  ('',20924544888,'',0,800,0,0);
 
 INSERT INTO user (name, identity, phone, in_come, credit_score, credit_amount, credit_status)
 VALUES  ('',20924544889,'',0,1500,0,0);
